41d47781dd27679f7734ddaa0f57c3223e39be7c
Naa'il Khokhar <74015139+therealnaail@users.noreply.github.com>
2023-02-13 08:06:38 +0000

    Update main.py
    In this improvement, the tkinter.messagebox module is used to show an info message when the notes are successfully saved or loaded.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import tkinter.messagebox as tkmb
 # Create the GUI window
 root = tk.Tk()
 root.title("Scribblespace")
@@ -13,6 +13,7 @@ notes_text.pack()
 def save_notes():
     with open("scribbles.txt", "w") as file:
         file.write(notes_text.get("1.0", "end"))
+    tkmb.showinfo("Scribblespace", "Notes saved successfully!")
 
 save_button = tk.Button(root, text="Save", command=save_notes, bg='light yellow')
 save_button.pack()
@@ -22,6 +23,7 @@ def load_notes():
     with open("scribbles.txt", "r") as file:
         notes_text.delete("1.0", tk.END)
         notes_text.insert("1.0", file.read())
+    tkmb.showinfo("Scribblespace", "Notes loaded successfully!")
 
 load_button = tk.Button(root, text="Load", command=load_notes, bg='light yellow')
 load_button.pack()
